96e9ab729fc3c116eec8897487168a6c0562f20e
Aarni Koskela <akx@iki.fi>
2014-12-28 20:49:08 +0200

    Release tooling

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var gutil = require('gulp-util');
 var browserify = require('gulp-browserify');
 var plumber = require('gulp-plumber');
 var concat = require('gulp-concat');
@@ -6,6 +7,10 @@ var uglify = require('gulp-uglify');
 var minifyCss = require('gulp-minify-css');
 var template = require('gulp-template');
 var fs = require('fs');
+var runSequence = require('run-sequence');
+var spawn = require('child_process').spawn;
+
+var RELEASE = false;
 
 gulp.task('lib', function () {
 	return gulp.src('src/libglitch/index.js')
@@ -15,6 +20,7 @@ gulp.task('lib', function () {
 			debug: false,
 			standalone: "Glitch",
 		}))
+		.pipe(RELEASE ? uglify() : gutil.noop())
 		.pipe(concat('libglitch.js'))
 		.pipe(gulp.dest('./build'));
 });
@@ -26,14 +32,14 @@ gulp.task('appjs', function () {
 			insertGlobals: false,
 			debug: false,
 		}))
-		.pipe(uglify())
+		.pipe(RELEASE ? uglify() : gutil.noop())
 		.pipe(concat('glitcher.js'))
 		.pipe(gulp.dest('./build'));
 });
 
 gulp.task('appcss', function () {
 	return gulp.src('src/glitcher/glitcher.css')
-		.pipe(minifyCss())
+		.pipe(RELEASE ? minifyCss() : gutil.noop())
 		.pipe(concat('glitcher.css'))
 		.pipe(gulp.dest('./build'));
 });
@@ -55,4 +61,21 @@ gulp.task('watchapp', ['app'], function () {
 	gulp.watch('src/**/*.js', ['app']);
 	gulp.watch('src/glitcher/template.html', ['app']);
 	gulp.watch('src/glitcher/glitcher.css', ['app']);
+});
+
+gulp.task("release=yes", function(complete) {
+	RELEASE = true;
+	complete();
+});
+
+gulp.task("gh", function(complete) {
+	runSequence('release=yes', 'app', function() {
+		var data = fs.readFileSync("dist/glitcher.html", "UTF-8");
+		data += '\n<script>!function(e,a,t,n,c,o,s){e.GoogleAnalyticsObject=c,e[c]=e[c]||function(){(e[c].q=e[c].q||[]).push(arguments)},e[c].l=1*new Date,o=a.createElement(t),s=a.getElementsByTagName(t)[0],o.async=1,o.src=n,s.parentNode.insertBefore(o,s)}(window,document,"script","//www.google-analytics.com/analytics.js","ga"),ga("create","UA-282164-17","auto"),ga("send","pageview");</script>';
+		spawn("git", ["checkout", "gh-pages"], {"stdio": "inherit"}).on("close", function(code) {
+			if(code) return;
+			fs.writeFileSync("./index.html", data, "UTF-8");
+			complete();
+		});
+	});
 });
