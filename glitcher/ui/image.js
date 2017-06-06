4288d97d014a06f37f93bafe2ebfcf3e502ae0b7
Aarni Koskela <akx@iki.fi>
2017-06-06 10:21:44 +0300

    Add resize for source image

--- glitcher/ui/image.js
+++ glitcher/ui/image.js
@@ -14,6 +14,31 @@ function loadImageFromFileField(event, complete) {
   fileReader.readAsDataURL(event.target.files[0]);
 }
 
+const resizePrompt = (ctrl) => {
+  const img = ctrl.engine.sourceImage;
+  const currSize = `${img.width}x${img.height}`;
+  let newSize;
+  if ((newSize = prompt('Enter the desired new size for the image.', currSize))) {  // eslint-disable-line no-alert
+    if (currSize === newSize) return;
+    let [_, width, height] = /^(\d+)\s*x\s*(\d+)$/i.exec(newSize);
+    width = parseInt(width, 10);
+    height = parseInt(height, 10);
+    if (!(width > 0 && height > 0)) {
+      alert('Unable to parse new size.');
+      return;
+    }
+    const tempCanvas = Object.assign(document.createElement('canvas'), {width, height});
+    const tempContext = tempCanvas.getContext('2d');
+    tempContext.drawImage(img, 0, 0, width, height);
+    const url = tempCanvas.toDataURL('image/png');
+    const newImage = new Image();
+    newImage.onload = () => {
+      ctrl.engine.sourceImage = newImage;
+    };
+    newImage.src = url;
+  }
+};
+
 const loadImageDiv = (ctrl) => (
   m(
     'div.image',
@@ -32,6 +57,7 @@ const loadImageDiv = (ctrl) => (
       }),
     ]),
     m('div', {style: 'opacity: .5'}, 'You can also paste image data from the clipboard.'),
+    m('button', {onclick: () => resizePrompt(ctrl)}, [m('i.fa.fa-arrows-alt'), ' Resize']),
   )
 );
 
