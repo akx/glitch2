a0d04c4a3e0cfe1faf2d8eeac5e4a8e5c343e494
Aarni Koskela <akx@iki.fi>
2018-04-03 16:42:07 +0300

    Add smooth and scale to xform

--- libglitch/modules/xform.js
+++ libglitch/modules/xform.js
@@ -12,24 +12,23 @@ function xform(glitchContext, options) {
   const context = glitchContext.getContext();
   context.globalAlpha = options.blend;
   context.globalCompositeOperation = options.operation;
+  context.imageSmoothingEnabled = options.smooth;
   context.setTransform(1, 0, 0, 1, 0, 0, 0);
+  const halfWidth = width / 2;
+  const halfHeight = height / 2;
+
   context.translate(
-    width / 2 + (options.xOffset / 100 * width),
-    height / 2 + (options.yOffset / 100 * height),
+    halfWidth + (options.xOffset / 100 * width),
+    halfHeight + (options.yOffset / 100 * height),
   );
   context.rotate(options.rotation * 0.0174533);
 
-  if (options.xFlip) {
-    context.scale(-1, 1);
-  }
-  if (options.yFlip) {
-    context.scale(1, -1);
-  }
-  context.drawImage(
-    context.canvas,
-    width * -0.5,
-    height * -0.5,
+  context.scale(
+    options.xScale * (options.xFlip ? -1 : 1),
+    options.yScale * (options.yFlip ? -1 : 1),
   );
+
+  context.drawImage(context.canvas, -halfWidth, -halfHeight);
   context.setTransform(1, 0, 0, 1, 0, 0);
   context.globalAlpha = 1;
   context.globalCompositeOperation = 'source-over';
@@ -37,22 +36,28 @@ function xform(glitchContext, options) {
 
 xform.paramDefaults = {
   blend: 1,
-  xFlip: false,
-  yFlip: false,
-  rotation: 0,
-  xOffset: 0,
-  yOffset: 0,
   operation: 'source-over',
+  rotation: 0,
+  smooth: true,
+  xFlip: false,
+  xOffset: 0,
+  xScale: 1,
+  yFlip: false,
+  yOffset: 0,
+  yScale: 1,
 };
 
 xform.params = [
   p.bool('xFlip'),
   p.bool('yFlip'),
   p.num('rotation', { min: -360, max: +360 }),
+  p.num('xScale', { min: 0, max: 50 }),
+  p.num('yScale', { min: 0, max: 50 }),
   p.num('xOffset', { min: -100, max: +100 }),
   p.num('yOffset', { min: -100, max: +100 }),
   p.num('blend', { description: 'blend' }),
   p.choice('operation', blendModes),
+  p.bool('smooth'),
 ];
 
 module.exports = xform;
