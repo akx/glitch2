24617c18d672107ac0f7cf987b62b1a8cf9f2091
Aarni Koskela <akx@iki.fi>
2018-04-04 15:28:06 +0300

    Refactor title UI out to module

--- glitcher/ui.js
+++ glitcher/ui.js
@@ -4,6 +4,7 @@ import stateUI from './ui/state';
 import fxUI from './ui/fx';
 import imageUI from './ui/image';
 import recorderUI from './ui/recorder';
+import titleUI from './ui/title';
 
 function controller() {
   this.state = null;
@@ -20,40 +21,12 @@ function controller() {
   this.gifRenderProgress = null;
 }
 
-const showHide = (ctrl, id, name) => (
-  m(
-    'a',
-    {
-      href: '#',
-      onclick: (event) => {
-        ctrl.ui[id] = !ctrl.ui[id];
-        event.preventDefault();
-      },
-    },
-    `${ctrl.ui[id] ? '-' : '+'} ${name}`
-  )
-);
-
-const titleDiv = ctrl => (
-  m(
-    'div.title',
-    [
-      m('a.by', { href: 'https://github.com/akx/glitch2', target: '_blank' }, 'glitcher by @akx'),
-      showHide(ctrl, 'stateMgmt', 'state'),
-      showHide(ctrl, 'recorder', 'recorder'),
-      showHide(ctrl, 'image', 'image'),
-      showHide(ctrl, 'fx', 'fx'),
-      showHide(ctrl, 'misc', 'misc'),
-    ],
-  )
-);
-
 function view(ctrl) {
   if (ctrl.engine === null) return null;
   // Slightly yucky side effect
   document.body.classList.toggle('zoom-canvas-to-fit', ctrl.ui.zoom);
   return m('div', [
-    titleDiv(ctrl),
+    titleUI(ctrl),
     (ctrl.ui.stateMgmt ? stateUI(ctrl) : null),
     (ctrl.ui.image ? imageUI(ctrl) : null),
     (ctrl.ui.recorder ? recorderUI(ctrl) : null),
---
+++ glitcher/ui/title.js
@@ -0,0 +1,31 @@
+import m from 'mithril';
+
+const showHide = (ctrl, id, name) => (
+  m(
+    'a',
+    {
+      href: '#',
+      onclick: (event) => {
+        ctrl.ui[id] = !ctrl.ui[id];
+        event.preventDefault();
+      },
+    },
+    `${ctrl.ui[id] ? '-' : '+'} ${name}`
+  )
+);
+
+const titleUI = ctrl => (
+  m(
+    'div.title',
+    [
+      m('a.by', { href: 'https://github.com/akx/glitch2', target: '_blank' }, 'glitcher by @akx'),
+      showHide(ctrl, 'stateMgmt', 'state'),
+      showHide(ctrl, 'recorder', 'recorder'),
+      showHide(ctrl, 'image', 'image'),
+      showHide(ctrl, 'fx', 'fx'),
+      showHide(ctrl, 'misc', 'misc'),
+    ],
+  )
+);
+
+export default titleUI;
